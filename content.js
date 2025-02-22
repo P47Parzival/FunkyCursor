0c36a8e0e26b3ffbc59aa56ebff62fb767ac192c
Dhruv Mali <158600867+P47Parzival@users.noreply.github.com>
2025-02-22 09:09:52 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,16 @@
-// all the tags of html "input, textarea, select, button, a, svg, img, div, span, p, h1, h2, h3, h4, h5, h6, ul, ol, li, table, tr, td, th, form, label, iframe, canvas, video, audio, embed, object, article, aside, details, figcaption, figure, footer, header, main, mark, nav, section, summary, time, blockquote, pre, code, kbd, samp, var, b, i, u, small, strong, em, abbr, cite, dfn, q, sub, sup, ins, del, s, address, bdo, bdi, ruby, rt, rp, wbr, area, map, track, meter, progress, output, datalist, keygen, command, menu, dialog"
+// document.querySelectorAll("input, textarea, select, button, a, svg, img, div, span, p, h1, h2, h3, h4, h5, h6, ul, ol, li, table, tr, td, th, form, label, iframe, canvas, video, audio, embed, object, article, aside, details, figcaption, figure, footer, header, main, mark, nav, section, summary, time, blockquote, pre, code, kbd, samp, var, b, i, u, small, strong, em, abbr, cite, dfn, q, sub, sup, ins, del, s, address, bdo, bdi, ruby, rt, rp, wbr, area, map, track, meter, progress, output, datalist, keygen, command, menu, dialog").forEach(element => {
+//   element.style.cursor = customCursor;
+// });
 
 function applyCursor(cursorFile) {
+  if (cursorFile === 'default') {
+    document.body.style.cursor = 'auto'; // Reset to default system cursor
+    document.querySelectorAll("*").forEach(element => {
+      element.style.cursor = 'auto';
+    });
+    return;
+  }
+
   const cursorUrl = chrome.runtime.getURL(cursorFile);
   console.log("Applying Cursor URL:", cursorUrl);
 
@@ -9,22 +19,21 @@ function applyCursor(cursorFile) {
   document.body.style.cursor = customCursor;
 
   document.querySelectorAll("input, textarea, select, button, a, svg, img, div, span, p, h1, h2, h3, h4, h5, h6, ul, ol, li, table, tr, td, th, form, label, iframe, canvas, video, audio, embed, object, article, aside, details, figcaption, figure, footer, header, main, mark, nav, section, summary, time, blockquote, pre, code, kbd, samp, var, b, i, u, small, strong, em, abbr, cite, dfn, q, sub, sup, ins, del, s, address, bdo, bdi, ruby, rt, rp, wbr, area, map, track, meter, progress, output, datalist, keygen, command, menu, dialog").forEach(element => {
-      element.style.cursor = customCursor;
+    element.style.cursor = customCursor;
   });
 }
 
-// Listen for messages (when user selects a cursor)
+// Listen for messages (when user selects or resets cursor)
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.cursor) {
-      chrome.storage.local.set({ selectedCursor: request.cursor }); // Save to storage
-      applyCursor(request.cursor);
+    chrome.storage.local.set({ selectedCursor: request.cursor });
+    applyCursor(request.cursor);
   }
 });
 
 // When a page loads, check for a stored cursor and apply it
 chrome.storage.local.get("selectedCursor", function (data) {
   if (data.selectedCursor) {
-      applyCursor(data.selectedCursor);
+    applyCursor(data.selectedCursor);
   }
 });
-
